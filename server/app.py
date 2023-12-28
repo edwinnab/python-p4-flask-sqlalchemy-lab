06791e8cb6ffabd735064b53c7c61f50ca1895ff
edwinna <edwinna>
2023-12-28 11:47:36 +0300

    sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,78 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    
+    if not animal:
+        response_data = "<h1>animal not found</h1>"
+        response = make_response(response_data, 404)
+        return response
+    response_data = f'''
+        <ul>
+        <li>ID:{animal.id}</li>
+        <li>Name: {animal.name}</li>
+        <li>Species: {animal.species}</li>
+        <li>Zookeeper: {animal.zookeeper.name}</li>
+        <li>Enclosure: {animal.enclosure.environment}</li>
+        </ul>
+    '''
+    response = make_response(response_data, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    
+    if not zookeeper:
+        response_data = "<h1>zookeper not found</h1>"
+        response = make_response(response_data, 404)
+        return response
+    
+    response_data = f'''
+        <ul>
+        <li>ID:{zookeeper.id}</li>
+        <li>Name: {zookeeper.name}</li>
+        <li>Birthday: {zookeeper.birthday}</li>
+        </ul>
+    '''
+    animals = Animal.query.filter(Zookeeper.id == Animal.zookeeper_id).all()
+    
+    for animal in animals:
+        response_data += f''' 
+            <ul>
+            <li>Animal: {animal.name}</li>
+            </ul>
+        '''
+    response = make_response(response_data, 200)   
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    if not enclosure:
+        response_data = "<h1>enclosure not found</h1>"
+        response = make_response(response_data, 404)
+        return response
+    
+    response_data = f'''
+        <ul>
+        <li>ID:{enclosure.id}</li>
+        <li>Environment: {enclosure.environment}</li>
+        <li>Open to Visitors: {enclosure.open_to_visitors}</li>
+        </ul>
+    '''
+    
+    animals = Animal.query.filter(Enclosure.id == Animal.enclosure_id).all()
+    
+    for animal in animals:
+        response_data += f''' 
+            <ul>
+            <li>Animal: {animal.name}</li>
+            </ul>
+        '''
+    response = make_response(response_data, 200)   
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,49 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    
+    animals = db.relationship('Animal',  back_populates='zookeeper')
+    
+    def __repr__(self):
+        return f"{self.id}, "\
+            +f"{self.name}, "\
+                +f"{self.birthday}"
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    
+    animals = db.relationship('Animal', back_populates='enclosure')
+    
+    def __repr__(self):
+        return f"{self.id}, "\
+            +f"{self.environment}, "\
+                +f"{self.open_to_visitors}"
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    
+    def __repr__(self):
+        return f"{self.id}, "\
+            +f"{self.name}, "\
+                +f"{self.species}"\
+                    +f"{self.zookeeper_id}"\
+                        +f"{self.enclosure_id}"
